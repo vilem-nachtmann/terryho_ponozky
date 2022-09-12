17af4160c5f257d2c1398f610cf511cfa72dcbf9
Vilém Nachtmann <56540691+vilem-nachtmann@users.noreply.github.com>
2022-09-12 11:52:47 +0200

    Update plakaty.py

--- plakaty.py
+++ plakaty.py
@@ -1,9 +1,9 @@
-from bs4 import BeautifulSoup
-import requests
+from bs4 import BeautifulSoup # Instalace: v příkazovém řádku zadejte "pip install beautifulsoup4"
+import requests # "pip install requests"
 
 velikost = input("zadejte hledanou velikost: ").lower()
 
-for i in range(8030,43186):
+for i in range(8030,43186): # Čísla v závorce = libovolně přepisovatelné rozmezí id plakátů, které program zkontroluje. 8030-43186 by mělo zahrnovat všechny plakáty.
     url = "https://www.terryhoponozky.cz/plakat/" + str(i)
 
     html_text = requests.get(url).text
